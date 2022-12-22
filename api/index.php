50725d9573bcbcf41287f72904c03ac82b0ced55
Pablo Mesquida <59099106+PabloMesquida@users.noreply.github.com>
2022-12-22 01:37:19 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,11 +3,11 @@ require 'spotify.php';
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json");
 $trackid = $_GET['trackid'];
-$url = $_GET['url'];
-$typed = $_GET['format'];
+//$url = $_GET['url'];
+//$typed = $_GET['format'];
 
 $re = '~[\bhttps://open.\b]*spotify[\b.com\b]*[/:]*track[/:]*([A-Za-z0-9]+)~';
-
+/*
 if (!$trackid && !$url) {
 	http_response_code(400);
 	$reponse = json_encode(["error" => true, "message" => "url or trackid parameter is required!", "usage" => "https://github.com/akashrchandran/spotify-lyrics-api"]);
@@ -17,7 +17,7 @@ if (!$trackid && !$url) {
 if ($url) {
 	preg_match($re, $url, $matches, PREG_OFFSET_CAPTURE, 0);
 	$trackid = $matches[1][0];
-}
+}*/
 $spotify = new Spotify();
 $spotify->check_if_expire();
 $reponse = $spotify->get_lyrics(track_id: $trackid);
